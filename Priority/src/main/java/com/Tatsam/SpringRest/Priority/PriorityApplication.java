8aea30eff1a243cece3ed7cd2934fc4a111e9fb4
VIJAY GUPTA <vijaygupta131999@gmail.com>
2021-01-24 01:15:43 +0530

    Initialize Repository

---
+++ Priority/src/main/java/com/Tatsam/SpringRest/Priority/PriorityApplication.java
@@ -0,0 +1,14 @@
+package com.Tatsam.SpringRest.Priority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityApplication.class, args);
+		System.out.println("Hi Tatsam");
+	}
+
+}
---
+++ Priority/src/test/java/com/Tatsam/SpringRest/Priority/PriorityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tatsam.SpringRest.Priority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
